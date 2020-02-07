d4b727cd339548eed324217e3715631f4105eaa7
Toan Phan <toanphan@berkeley.edu>
2020-02-07 05:00:53 -0800

    update

--- Hello.java
+++ Hello.java
@@ -3,6 +3,19 @@
 
 public class Hello {
 	public static void main(String[] args) {
-		System.out.println("Hello World! How are you? My name is " + args.toString());
+		System.out.println("Hello World! How are you? My name is Toan Phan.");
+
+		//Three dimensional array
+		int[][][] arr = {
+							{{1, 2}, {3, 4}},
+							{{5, 6}, {7, 8}}
+						};
+		for (int a = 0; a < arr.length; a ++) {
+			for (int b = 0; b < arr[a].length; b++) {
+				for (int c = 0; c < arr[a][b].length; c++) {
+					System.out.println("Hello " + arr[a][b][c]);
+				}
+			}
+		}
 	}
 }
