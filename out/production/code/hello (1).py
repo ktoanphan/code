9647dce112ddc6ef62ec468ad4771476bc9c6a86
Toan Phan <toanphan@berkeley.edu>
2020-05-02 16:19:04 -0700

    folder updates

---
+++ out/production/code/hello (1).py
@@ -0,0 +1,26 @@
+
+def egcd(x, y):
+    """extended euclid algorithm."""
+    d = 1
+    a, b 
+
+    x = 1*x + 0*y
+    y = 0*x + 1*y
+
+    print(x)
+    print(y)
+    print(d)
+
+    while (y < x - y): 
+        x = x - y
+        d += 1
+
+
+    while (d >= 1): 
+        a = 1
+        b = ()
+    print(x)
+    print(y)
+    print(d)
+
+
